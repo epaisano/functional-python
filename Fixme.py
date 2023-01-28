4e663f639576f61c0d33a92febd68f068c6f2908
Emma Munoz <epaisano24@cmc.edu>
2023-01-28 15:35:14 -0800

    finished code

--- Fixme.py
+++ Fixme.py
@@ -5,9 +5,11 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
-    Returns a list of even numbers from 0 to n inclusive.
+    Returns a list of even numbers
+    from 0 to n inclusive.
 
     >>> evens(10)
     [0, 2, 4, 6, 8, 10]
@@ -20,14 +22,16 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    evens = lambda x: x%2 == 0
     xs = range(n+1)
-    xs = map(lambda list(evens, xs)
-   print("xs =", xs )
+    xs = filter(lambda x: x % 2 == 0, xs)
+    xs = list(xs)
+    return xs
+
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from
+    0 to n inclusive that contain the digit 3.
 
     >>> threes(2)
     []
@@ -40,16 +44,21 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    threes = lambda x: x == '3'
-
+    xs = range(n+1)
+    xs = filter(lambda x: '3' in str(x), xs)
+    xs = map(int, xs)
+    xs = list(xs)
+    return xs
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 4 characters long.
+    Returns a list of all words in the input
+    text that are less than 4 characters long.
 
     HINT:
-    Recall that text.split() converts the text variable into a list of words.
+    Recall that text.split() converts the text
+    variable into a list of words.
 
     >>> small_words('this is a simple test case')
     ['this', 'is', 'a', 'test', 'case']
@@ -60,10 +69,10 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    small_words = lambda x: len(x) < 4
-    xs = text.split(text)
-    xs = filter(small_words, xs)
-    print("xs =", xs)
+    xs = text.split()
+    xs = filter(lambda x: len(x) < 5, xs)
+    xs = list(xs)
+    return xs
 
 
 def squares(n):
@@ -80,16 +89,16 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    squares = lambda x: x**2
-    xs = range(n+1)
-    xs = map(ssquares, xs)
+    xs = range(1, n+1)
+    xs = map(lambda x: x**2, xs)
     xs = list(xs)
-    print("xs =", xs)
+    return xs
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of
+    the lengths of the corresponding strings.
 
     >>> lengths([])
     []
@@ -98,8 +107,7 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
-    lengths = lambda x: len(x)
     xs = strings
-    xs = map(lengths, xs)
+    xs = map(lambda x: len(x), xs)
     xs = list(xs)
-    print("xs=", xs)
+    return xs
